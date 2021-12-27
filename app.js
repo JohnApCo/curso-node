fb2dc2e73a163a832214d9504c568e09b137dd84
JohnApCo <john.apco.dev@gmail.com>
2021-12-26 21:08:00 -0500

    fudamentos de javascript y node

---
+++ app.js
@@ -0,0 +1,15 @@
+const frutas = require("./frutas");
+
+const cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "I'm a moooodule",
+    e: "Oo",
+    T: "U",
+  })
+);
+
+frutas.forEach((fruta) => {
+  console.count(fruta);
+});
---
+++ index.js
@@ -0,0 +1,67 @@
+/*const john = {
+  nombre: "john",
+  apellido: "apaza",
+  edad: 26
+};
+
+let { nombre, edad, apellido } = john;
+console.log(
+  `mis datos son: nombre=${nombre}, edad=${edad} apellido=${apellido}`
+);*/
+
+//fetch
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.results);
+    data.results.forEach((element) => {
+      console.log(element.name);
+    });
+  })
+  .catch((error) => console.log(error));
+
+console.log("Lista de pokemones");
+*/
+//async await
+/*
+const obtenerPokemones = async () => {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await res.json();
+    console.log(data.results);
+    data.results.forEach((element) => {
+      console.log(element.name);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+obtenerPokemones();
+*/
+//Map
+const obtenerPokemones = async () => {
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const data = await res.json();
+      console.log(data.results);
+      let arrayNombre=data.results.map(poke => poke.name)
+      console.log(arrayNombre);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  obtenerPokemones();
+  //Filter
+const obtenerPokemones = async () => {
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const data = await res.json();
+      console.log(data.results);
+      let arrayNombre=data.results.filter(poke => poke.name==='bulbasour')
+      console.log(arrayNombre);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  obtenerPokemones();
